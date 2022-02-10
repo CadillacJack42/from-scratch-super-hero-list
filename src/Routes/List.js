5a4b20971cd17ed7d4803d59b28c80d8d3bf8429
James Keicher <jamesdkeicher@gmail.com>
2022-02-10 14:22:33 -0800

    feat: filter heroes by name, publisher, alignment

--- src/Routes/List.js
+++ src/Routes/List.js
@@ -2,11 +2,14 @@ import Hero from '../Hero';
 import '../Styles/List.css';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { getAllHeroes } from '../services/fetch-utils';
+import { filterHeroes, getAllHeroes } from '../services/fetch-utils';
 
 export default function List({ setDisabled }) {
   const { page } = useParams();
   const [heroes, setHeroes] = useState([]);
+  const [column, setColumn] = useState('name');
+  const [query, setQuery] = useState('');
+
   const From = (page - 1) * 50;
   const To = page * 50 - 1;
 
@@ -18,11 +21,36 @@ export default function List({ setDisabled }) {
     };
     myFunc();
   }, [From, To, setDisabled]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const filteredHeroes = await filterHeroes(column, query);
+    setHeroes(filteredHeroes);
+    console.log(filteredHeroes);
+  };
+
   return (
-    <div className="hero-container">
-      {heroes.map((hero) => {
-        return <Hero hero={hero} key={hero.slug} />;
-      })}
+    <div>
+      <div className="search-heroes">
+        <label>
+          Find Hero By :
+          <form onSubmit={handleSubmit}>
+            <select onChange={(e) => setColumn(e.target.value)}>
+              <option value="name">Name</option>
+              <option value="publisher">Publisher</option>
+              <option value="alignment">Alignment</option>
+            </select>
+            <input value={query} onChange={(e) => setQuery(e.target.value)}></input>
+            <button>Search</button>
+          </form>
+        </label>
+      </div>
+
+      <div className="hero-container">
+        {heroes.map((hero) => {
+          return <Hero hero={hero} key={hero.slug} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -11,3 +11,8 @@ export const getHeroById = async (id) => {
 
   return response.data;
 };
+export const filterHeroes = async (column, query) => {
+  const response = await client.from('super_heroes').select().textSearch(column, `${query}`);
+  console.log(response);
+  return checkError(response);
+};
