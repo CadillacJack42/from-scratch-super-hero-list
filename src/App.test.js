cc8a75361a2ca8cdfb391bd9225d21895f9fe8a0
James Keicher <jamesdkeicher@gmail.com>
2022-02-10 16:22:49 -0800

    test: match on catalogue

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/hero/i);
+  const linkElement = screen.getByText(/catalogue/i);
   expect(linkElement).toBeInTheDocument();
 });
