da54abd52629a95daa285742aee993c21ce1ae4b
James Keicher <jamesdkeicher@gmail.com>
2022-02-13 02:30:58 -0800

    feat: HeroDetail converted to class component

--- src/HeroDetail.js
+++ src/HeroDetail.js
@@ -1,74 +1,83 @@
 import React from 'react';
 import './Styles/HeroDetail.css';
 
-export default function HeroDetail({ hero }) {
-  return (
-    <div className="hero-detail-container">
-      <h1 className="hero-name">{`${hero.name} `}</h1>
-      <div className="center-items">
-        <h3 className="names">
-          {`Name : ${hero.name}`}
-          <br></br>
-          {`Full Name : ${hero.full_name}`}
-          <br></br>
-          {`Known Aliases : ${hero.aliases}`}
-          <br></br>
-          {`Alter Egos : ${hero.alter_egos}`}
-          <br></br>
-        </h3>
-        <img alt={'No Image Available'} src={hero.image} className={'hero-detail-image'}></img>
-        <div className="hero-stats">
-          <h3>{hero.name} Stats</h3>
-          <p>
-            {`Combat: ${hero.combat}`}
+export default class HeroDetail extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="hero-detail-container">
+        <h1 className="hero-name">{`${this.props.hero.name} `}</h1>
+        <div className="center-items">
+          <h3 className="names">
+            {`Name : ${this.props.hero.name}`}
             <br></br>
-            {`Power: ${hero.power}`}
+            {`Full Name : ${this.props.hero.full_name}`}
             <br></br>
-            {`Durability: ${hero.durability}`}
+            {`Known Aliases : ${this.props.hero.aliases}`}
             <br></br>
-            {`Speed: ${hero.speed}`}
+            {`Alter Egos : ${this.props.hero.alter_egos}`}
             <br></br>
-            {`Strength: ${hero.strength}`}
+          </h3>
+          <img
+            alt={'No Image Available'}
+            src={this.props.hero.image}
+            className={'hero-detail-image'}
+          ></img>
+          <div className="hero-stats">
+            <h3>{this.props.hero.name} Stats</h3>
+            <p>
+              {`Combat: ${this.props.hero.combat}`}
+              <br></br>
+              {`Power: ${this.props.hero.power}`}
+              <br></br>
+              {`Durability: ${this.props.hero.durability}`}
+              <br></br>
+              {`Speed: ${this.props.hero.speed}`}
+              <br></br>
+              {`Strength: ${this.props.hero.strength}`}
+              <br></br>
+              {`Intelligence: ${this.props.hero.intelligence}`}
+            </p>
+          </div>
+        </div>
+
+        <h3>{this.props.hero.name} Bio</h3>
+        <div className="bio-container">
+          <p className="bio-left">
+            {`Publisher : ${this.props.hero.publisher}`}
             <br></br>
-            {`Intelligence: ${hero.intelligence}`}
+            {`First Appearance : ${this.props.hero.first_appearance}`}
+            <br></br>
+            {`alignment : ${this.props.hero.alignment}`}
+            <br></br>
+            {`Occupation : ${this.props.hero.occupation}`}
+            <br></br>
+            {`Base : ${this.props.hero.base}`}
+            <br></br>
+            {`Groups : ${this.props.hero.groups}`}
+            <br></br>
+            {`Relatives : ${this.props.hero.relatives}`}
+            <br></br>
+          </p>
+          <p className="bio">
+            {`Race : ${this.props.hero.race}`}
+            <br></br>
+            {`Gender : ${this.props.hero.gender}`}
+            <br></br>
+            {`Birthplace : ${this.props.hero.birthplace}`}
+            <br></br>
+            {`Height : ${this.props.hero.height}`}
+            <br></br>
+            {`Weight : ${this.props.hero.weight}`}
+            <br></br>
+            {`Hair : ${this.props.hero.hair_color}`}
+            <br></br>
+            {`Eyes : ${this.props.hero.eye_color}`}
           </p>
         </div>
       </div>
-
-      <h3>{hero.name} Bio</h3>
-      <div className="bio-container">
-        <p className="bio-left">
-          {`Publisher : ${hero.publisher}`}
-          <br></br>
-          {`First Appearance : ${hero.first_appearance}`}
-          <br></br>
-          {`alignment : ${hero.alignment}`}
-          <br></br>
-          {`Occupation : ${hero.occupation}`}
-          <br></br>
-          {`Base : ${hero.base}`}
-          <br></br>
-          {`Groups : ${hero.groups}`}
-          <br></br>
-          {`Relatives : ${hero.relatives}`}
-          <br></br>
-        </p>
-        <p className="bio">
-          {`Race : ${hero.race}`}
-          <br></br>
-          {`Gender : ${hero.gender}`}
-          <br></br>
-          {`Birthplace : ${hero.birthplace}`}
-          <br></br>
-          {`Height : ${hero.height}`}
-          <br></br>
-          {`Weight : ${hero.weight}`}
-          <br></br>
-          {`Hair : ${hero.hair_color}`}
-          <br></br>
-          {`Eyes : ${hero.eye_color}`}
-        </p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
